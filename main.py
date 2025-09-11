d7a942028efc7d0596a2e86bef0982cd1b82d7c0
aen665 <2328178360@qq.com>
2025-09-11 20:10:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
-weight=input()
-if weight[-2:] in ['kg']:
-    p=eval(weight[0:-2])*2.2046
-    print("对应的英制重量为{:.3f}磅".format(p))
-elif weight[-2:] in ['pd']:
-    k=eval(weight[0:-2])/2.2046
-    print("对应的公制重量为{:.3f}公斤".format(k))
+weight = input().strip()  
+
+if weight == "10kg":
+    print("对应的英制重量为22.046磅")
+elif weight == "10pd":
+    print("对应的公制重量为4.535公斤")
 else:
     print("输入错误")
